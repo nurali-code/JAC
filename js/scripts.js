38ace9343de576a0796e9e852aa935360120ac4f
Nurali Davlatov <nur3.dav.97@gmail.com>
2023-05-16 17:12:59 +0500

    email

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,50 @@ $(document).ready(function () {
 
     /*---------------------------------------------------end*/
 
+    function hideModals() {
+        $('.modal').fadeOut();
+        $('.modal, body, [data-modal]').removeClass('active');
+    };
+    $(function () {
+        function showModal(id) {
+            if ($(id).hasClass('active')) {
+                $(id).fadeOut(300)
+                $(id).removeClass('active');
+                $('body').removeClass('active');
+            } else {
+                $(id).addClass('active')
+                $('body').addClass('active');
+                $(id).fadeIn(300);
+            }
+        }
+
+        $('[data-modal]').on('click', function (e) {
+            e.preventDefault();
+            $(this).toggleClass('active')
+            showModal('#' + $(this).attr("data-modal"));
+        });
+
+        $('.modal-close').on('click', () => {
+            hideModals();
+        });
+
+        $(document).on('click', function (e) {
+            if (!(
+                ($(e.target).parents('.modal-content').length) ||
+                ($(e.target).parents('.nav').length) ||
+                ($(e.target).parents('.btn-menu').length) ||
+                ($(e.target).hasClass('nav')) ||
+                ($(e.target).hasClass('btn-menu')) ||
+                ($(e.target).hasClass('btn')) ||
+                ($(e.target).hasClass('modal-content'))
+            )) {
+                hideModals();
+            }
+        });
+    });
+
+    /*---------------------------------------------------end*/
+
     $('a[href*="#"]').on('click', function (e) {
         e.preventDefault();
         $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top - 40, }, 300,)
@@ -50,6 +94,7 @@ $(document).ready(function () {
             },
         ]
     });
+
     $('.guarantee-slider').slick({
         dots: false,
         arrows: true,
--- sendmail.php
+++ sendmail.php
@@ -5,15 +5,12 @@ require_once 'PHPMailer/src/Exception.php';
 
 $name = $_POST['name'];
 $phone = $_POST['phone_number'];
-
-$service = $_POST['service'];
 $capacity = $_POST['capacity'];
 $height = $_POST['height'];
 $equipment = $_POST['equipment'];
 $сabin = $_POST['сabin'];
 $frame = $_POST['frame'];
 $offcet = $_POST['offcet'];
-$type = $_POST['type'];
 $hours = $_POST['hours'];
 
 use PHPMailer\PHPMailer\PHPMailer;
@@ -34,7 +31,7 @@ try {
     $mail->Port       = 587;
 
     $mail->setFrom('info@jac-elektro.ru', 'ЭЛЕКТРОМОБИЛЬНЫЙ ПОГРУЗЧИК JAC');
-    $mail->addAddress('nur3.dav.97@gmail.com', 'Получатеь');
+    $mail->addAddress('info@jacvostok.ru', 'Получатеь');
     $mail->addReplyTo('info@jac-elektro.ru', 'ЭЛЕКТРОМОБИЛЬНЫЙ ПОГРУЗЧИК JAC');
 
     $mail->isHTML(true);
@@ -43,18 +40,22 @@ try {
     if (!empty($hours)) {
         $hours_row = '
         <tr>
-            <td style="border: 1px solid #bdbdbd; padding: 5px; width: 180px">Количество часов</td>
-            <td style="border: 1px solid #bdbdbd; padding: 5px;">' . $hours . '</td>
+            <td style="border: 1px solid #bdbdbd; padding: 5px; width: 180px">Грузоподёмность</td>
+            <td style="border: 1px solid #bdbdbd; padding: 5px;">' . $capacity . '</td>
+        </tr>
+        <tr>
+            <td style="border: 1px solid #bdbdbd; padding: 5px; width: 180px">Высота</td>
+            <td style="border: 1px solid #bdbdbd; padding: 5px;">' . $height . '</td>
+        </tr>
+        <tr>
+            <td style="border: 1px solid #bdbdbd; padding: 5px; width: 180px">Дополнительно</td>
+            <td style="border: 1px solid #bdbdbd; padding: 5px;">' . $frame . '<br>' . $equipment .  '<br>' . $сabin . '<br>' . $offcet . '</td>
         </tr>';
     } else {
         $hours_row = '';
     }
 
     $mail->Body = ' <table style="width: 100%;">
-        <tr>
-            <td style="border: 1px solid #bdbdbd; padding: 5px; width: 180px">Услуга</td>
-            <td style="border: 1px solid #bdbdbd; padding: 5px;">' . $service . '</td>
-        </tr>
         <tr>
             <td style="border: 1px solid #bdbdbd; padding: 5px; width: 180px">Имя</td>
             <td style="border: 1px solid #bdbdbd; padding: 5px;">' . $name . '</td>
